48685f6ded5432b01e951fef569f1fc535f5fd7b
Romain Lopez <romain_lopez@berkeley.edu>
2023-05-12 17:49:27 -0700

    Update _module.py

--- svae/_module.py
+++ svae/_module.py
@@ -363,7 +363,7 @@ class SpikeSlabVAEModule(BaseModuleClass):
             # Implementation detail described in the paper: line below describes the mathematical derivations in the paper
             # kl_global = torch.tensor(0.0) + kl_discrete - logp_w
             # Line below is the practical implementation, setting p_discrete to q_discrete
-            # kl_global = torch.tensor(0.0) - logp_qw
+            kl_global = torch.tensor(0.0) - logp_qw
             loss = (
                 n_obs * torch.mean(reconst_loss + weighted_kl_local)
                 + kl_weight * kl_global
